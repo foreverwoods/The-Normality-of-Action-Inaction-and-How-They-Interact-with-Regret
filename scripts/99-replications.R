3d45585f6af02e7770ffc6f61b091b041b8ae92f
foreverwoods <j2hashz0p@outlook.com>
2024-02-15 15:56:21 +0000

    completed data and abstract, as well as replication table

--- scripts/99-replications.R
+++ scripts/99-replications.R
@@ -1,19 +1,62 @@
 #### Preamble ####
-# Purpose: Replicated graphs from... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Replicate graphs from Feldman's research paper
+# Date: February 15th, 2024
+# Contact: janssen.rambaud@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: None
 
 
-#### Workspace setup ####
+# Load necessary libraries
 library(tidyverse)
-# [...UPDATE THIS...]
 
-#### Load data ####
-# [...ADD CODE HERE...]
+# Read the data from the CSV file
+exp2_data <- read.csv("data/analysis_data/analysis_data_exp2.csv", header = TRUE, na.strings = "NA")
 
+# Transform the data into the desired format
+
+exp2_data_table <- tibble(
+  norms = c("Action norms", "Inaction norms", "Control norms"),
+  
+  M_AP_Bvr = 
+    c(mean(colMeans(exp2_data[c("aa_regret")], na.rm = TRUE)),
+      mean(colMeans(exp2_data[c("ia_regret")], na.rm = TRUE)),
+      mean(colMeans(exp2_data[c("ca_regret")], na.rm = TRUE))
+      ),
+  
+  SD_AP_Bvr = 
+    c(apply(exp2_data[c("aa_regret")], 2, sd, na.rm = TRUE),
+      apply(exp2_data[c("ia_regret")], 2, sd, na.rm = TRUE),
+      apply(exp2_data[c("ca_regret")], 2, sd, na.rm = TRUE)
+    ),
+  
+  N_AP_Bvrr = 
+    c(sum(!is.na(exp2_data[,c("aa_norms")])),
+      sum(!is.na(exp2_data[,c("ia_norms")])),
+      sum(!is.na(exp2_data[,c("ca_norms")]))
+      ),
+  
+  M_IP_Bvr = 
+    c(mean(colMeans(exp2_data[c("ai_regret")], na.rm = TRUE)),
+      mean(colMeans(exp2_data[c("ii_regret")], na.rm = TRUE)),
+      mean(colMeans(exp2_data[c("ci_regret")], na.rm = TRUE))
+    ),
+  
+  SD_IP_Bvr = 
+    c(apply(exp2_data[c("ai_regret")], 2, sd, na.rm = TRUE),
+      apply(exp2_data[c("ii_regret")], 2, sd, na.rm = TRUE),
+      apply(exp2_data[c("ci_regret")], 2, sd, na.rm = TRUE)
+    ),
+  
+  N_IP_Bvr = 
+    c(sum(!is.na(exp2_data[,c("ai_norms")])),
+      sum(!is.na(exp2_data[,c("ii_norms")])),
+      sum(!is.na(exp2_data[,c("ci_norms")]))
+    )
+)
+
+# tibble
+
+print(exp2_data_table)
+write.table(exp2_data_table, "data/analysis_data/exp2_tibble.csv", sep=",", row.names = FALSE)
 
 
